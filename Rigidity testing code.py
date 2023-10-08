b56eb5f68559f214d555522bd34b0300f2fe17f5
Ritwik Anand <amritwik@gmail.com>
2023-10-08 23:03:26 +0100

    Slow progress

--- Rigidity testing code.py
+++ Rigidity testing code.py
@@ -1,22 +1,29 @@
 # Code to test the rigidity of a graph
 # plan is to create a networkx graph and then, for each node, randomly assign 
 # a point onto the x-y plane so that it can be analysed
+# use seed to ensure reproducibility 
 
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 plt.figure()
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 1, 4, 5]
+x = range(10)
+y = range(3, 22, 2)
+
+for i in range(9):
+    line_x = [x[i], x[i + 1]]
+    line_y = [y[i], y[i + 1]]
+    plt.plot(line_x, line_y, 'k-')  
 
 # Create a scatter plot
-plt.scatter(x, y)
+plt.plot(x, y, 'bo')
+plt.plot(line_x, line_y, 'k-')
 
 # Add labels and a title
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Scatter Plot of Points')
 
 # Show the plot
 plt.show()
