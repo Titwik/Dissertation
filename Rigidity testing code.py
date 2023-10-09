a42000c704e095e01bae23bb19d0ff56be4b34fd
Ritwik Anand <amritwik@gmail.com>
2023-10-09 23:57:18 +0100

    Graph onto x-y plane

--- Rigidity testing code.py
+++ Rigidity testing code.py
@@ -8,22 +8,49 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-plt.figure()
-x = range(10)
-y = range(3, 22, 2)
+# set the seed for reproducibility
+random.seed(42)
 
-for i in range(9):
-    line_x = [x[i], x[i + 1]]
-    line_y = [y[i], y[i + 1]]
-    plt.plot(line_x, line_y, 'k-')  
+####################################################################################################
 
-# Create a scatter plot
-plt.plot(x, y, 'bo')
-plt.plot(line_x, line_y, 'k-')
+# create a graph first
+G = nx.complete_graph(5)
+G.add_edge(2,5)
+nx.draw_networkx(G)
 
-# Add labels and a title
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+####################################################################################################
 
-# Show the plot
-plt.show()
+# create a function that plots the graph on the x-y plane
+def G_plane(G):
+    num_of_nodes = len(G.nodes)
+    x_nodes = []
+    y_nodes = []
+    
+    # create random coordinates for each node in the graph
+    for i in range(num_of_nodes):
+        x = random.uniform(-10, 10)
+        y = random.uniform(-10, 10)
+        x_nodes.append(x)
+        y_nodes.append(y)
+    
+    # Plot the graph
+    plt.figure()
+    
+    # Plot the nodes onto the graph
+    plt.plot(x_nodes, y_nodes, 'bo', markersize=15)
+    
+    # Add labels to the points
+    for i, (x, y) in enumerate(zip(x_nodes, y_nodes)):
+        plt.text(x, y, str(i), fontsize=12, ha='center', va='center', color='white')
+    
+    # Add the edges between the corresponding nodes
+    edges = list(G.edges)
+    for i in range(len(edges)):
+        point_1 = [x_nodes[edges[i][0]], x_nodes[edges[i][1]]]
+        point_2 = [y_nodes[edges[i][0]], y_nodes[edges[i][1]]]
+        plt.plot(point_1, point_2, 'k-')
+    
+    plt.show()
+
+    
+G_plane(G)
